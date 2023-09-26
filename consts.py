6540c83623d7557aa96841d83b90a9a863b2704f
asher-epstein-42 <110289469+asher-epstein-42@users.noreply.github.com>
2023-09-26 13:46:41 +0300

    Update consts.py

--- consts.py
+++ consts.py
@@ -1,3 +1,5 @@
+IMAGE_TYPES = ('bmp', 'dng', 'jpeg', 'jpg', 'mpo', 'png', 'tif', 'tiff', 'webp', 'pfm')
+
 NAMES = {0: 'person', 1: 'bicycle', 2: 'car', 3: 'motorcycle', 4: 'airplane', 5: 'bus', 6: 'train', 7: 'truck',
         8: 'boat', 9: 'traffic light', 10: 'fire hydrant', 11: 'stop sign', 12: 'parking meter', 13: 'bench',
         14: 'bird', 15: 'cat', 16: 'dog', 17: 'horse', 18: 'sheep', 19: 'cow', 20: 'elephant', 21: 'bear', 22: 'zebra',
